b0f50ce43e8b354bb837b66fa7c197b2a08ab0f1
aaron snobel <aaronsnobel@aaronsnobel.ca>
2018-08-14 18:10:59 -0400

    Deps fix

--- StyledComponents.js
+++ StyledComponents.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'; // 3.4.2
-import { View, Text, FlatList } from "react-native";
-export const HeaderPhoto = require('./assets/lgoo@3x.png');
+import { View, Text,Image, FlatList } from "react-native";
+export const HeaderPhoto = require('./assets/logo@3x.png');
 export const GemPhoto = require('./assets/gem@3x.png');
 export const SendPhoto = require('./assets/send@3x.png');
 export const CommentPhoto = require('./assets/comments@3x.png');
@@ -8,11 +8,11 @@ export const BookmarkPhoto = require('./assets/bookmark@3x.png');
 export const BoldAuthor = styled(Text)`
 font-weight:bold;
 `;
-export const StyledImg = styled.img`
+export const StyledImg = styled(Image)`
 height:200px;
 object-fit: cover;
 `;
-export const HeaderImage = styled.img`
+export const HeaderImage = styled(Image)`
 height:100%;
 object-fit:cover;
 `
@@ -32,6 +32,6 @@ export const ActionItem = styled(View)`
 flex:0 0 33%;
 overflow:hidden;
 `
-export const ActionImage = styled.img`
+export const ActionImage = styled(Image)`
 height:24px;
 `
